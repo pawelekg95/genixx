c7ce49bc4e174f06de987626c9cf04d1b287554b
Paweł Gmurczyk <56723306+pawelekg95@users.noreply.github.com>
2024-01-07 20:29:57 +0100

    Update rethreadme.cmake (#17)

--- lib/rethreadme.cmake
+++ lib/rethreadme.cmake
@@ -2,12 +2,11 @@ include(FetchContent)
 FetchContent_Declare(
     rethreadme
     GIT_REPOSITORY  https://github.com/pawelekg95/reThreadMe.git
-    GIT_TAG         9405727b385b3be1d7db52906b119987343c5316
+    GIT_TAG         ad3faa69b5763c73d5825d53c53aea2d308c9aab
 )
 
 FetchContent_GetProperties(rethreadme)
 if (NOT rethreadme_POPULATED)
     FetchContent_Populate(rethreadme)
+    add_subdirectory(${rethreadme_SOURCE_DIR}/lib ${rethreadme_BINARY_DIR})
 endif ()
-
-add_subdirectory(${rethreadme_SOURCE_DIR}/lib ${rethreadme_BINARY_DIR})
